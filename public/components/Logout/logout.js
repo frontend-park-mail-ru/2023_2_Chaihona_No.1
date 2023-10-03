56cd51363adb1e4d0e4b367a91a643b053a3458d
UmalatDukuev <dukuev037@mail.ru>
2023-10-03 22:17:06 +0000

    dev: erase the error porcess

--- public/components/Logout/logout.js
+++ public/components/Logout/logout.js
@@ -3,12 +3,7 @@ import navbar from "../Navbar/Navbar.js";
 
 export default async() => {
   const api = new Api();
-  const result = await api.logout();
-  if (result.status >= 400) {
-      const err = document.querySelector(".error");
-      err.textContent = "Произошла ошибка";
-      return;
-  }
+  await api.logout();
   navbar();
   window.router.redirect('/')
 }
