dcd61aeea7240b4b458e842e03892ab8545ff7b9
UmalatDukuev <dukuev037@mail.ru>
2023-10-05 20:42:04 +0000

    Revert "one js file in html"
    This reverts commit 3c348165132d1bb44378efca718dd01069a177e0.

--- public/index.js
+++ public/index.js
@@ -1,16 +1,6 @@
 import {Router} from "./modules/router.js";
 import navbar from "./components/Navbar/Navbar.js";
 import {Api} from "./modules/api.js";
-importScripts('./modules/requests.js', './configs/link_config.js', './configs/rest_config.js',
-            './components/UserProfile/uprofile-prec.js', './components/Target/target-prec.js',
-            './components/SubLevel/sub_level-prec.js', './components/Register/register-prec.js',
-            './components/Register/register.js', './components/Post/post-prec.js',
-            './components/NotFound/notfound.js', './components/NotFound/notfound-prec.js',
-            './components/Navbar/Navbar.js', './components/Logout/logout.js',
-            './components/Login/login.js', './components/Login/login-prec.js',
-            './components/Comment/comment-prec.js', './components/AuthorProfile/profile.js',
-            './components/AuthorProfile/aprofile-prec.js'
-)
 
 const api = new Api();
 const isAuth = await api.isAuth();
