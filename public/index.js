a208dfd19657687ae9445d4123b051cfe6844916
dnonakolesax <90285641+dnonakolesax@users.noreply.github.com>
2023-10-19 00:24:09 +0300

    fix PR issues

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import { Router } from './modules/router.js'; 
+import { Router } from './modules/router.js';
 import navbar from './components/Navbar/Navbar.js';
 import { Api } from './modules/api.js';
 import { precompiledDir, precompiledFnames } from './configs/precompiled_config.js';
@@ -6,33 +6,31 @@ import { precompiledDir, precompiledFnames } from './configs/precompiled_config.
 const router = new Router();
 window.router = router;
 
-function loadScript (scriptName) {
-    var script = document.createElement('script')
-    script.src = scriptName;  
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
+function loadScript(scriptName) {
+  const script = document.createElement('script');
+  script.src = scriptName;
+  script.type = 'text/javascript';
+  document.head.appendChild(script);
 }
 
-
-function loadPrecompiled () {
-    precompiledFnames.forEach(function(precompiledFname){
-        loadScript(precompiledDir + precompiledFname);  
-    })  
+function loadPrecompiled() {
+  precompiledFnames.forEach((precompiledFname) => {
+    loadScript(precompiledDir + precompiledFname);
+  });
 }
 
-
-async function init () {
-    const api = new Api();
-    const isAuth = await api.isAuth();
-    if (isAuth.data.body.is_authorized === true) {
-        navbar({ User: { id: '1' } });
-        router.redirect('/profile1');
-      } else {
-        navbar();
-        router.redirect('/login');
-    }
+async function init() {
+  const api = new Api();
+  const isAuth = await api.isAuth();
+  if (isAuth.data.body.is_authorized === true) {
+    navbar({ User: { id: '1' } });
+    router.redirect('/profile1');
+  } else {
+    navbar();
+    router.redirect('/login');
+  }
 }
 
-loadScript('handlebars/dist/handlebars.js');
+// loadScript('handlebars/dist/handlebars.js');
 loadPrecompiled();
 init();
