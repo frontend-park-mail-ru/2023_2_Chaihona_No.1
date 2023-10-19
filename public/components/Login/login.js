f53dcde7a7a70642e4a2e6ce2573855d7c700f48
Umalat <dukuev037@mail.ru>
2023-10-20 01:54:51 +0300

    added classes

--- public/components/Login/login.js
+++ public/components/Login/login.js
@@ -1,8 +1,27 @@
-
-
+import {Api} from "../../modules/api.js";
+import navbar from "../Navbar/Navbar.js";
 
 export default async() => {
     const rootElement = document.querySelector('#root');
     rootElement.innerHTML = '';
     rootElement.innerHTML = Handlebars.templates.login();
+
+
+    const loginBtn = document.querySelector(".logbutton")
+    loginBtn.addEventListener("click", async function (e) {
+        e.preventDefault();
+        const login = document.querySelector("#login");
+        const pass = document.querySelector("#password");
+        const api = new Api();
+        const result = await api.login(login.value, pass.value);
+        if (result.status >= 400) {
+            const err = document.querySelector(".error");
+            err.textContent = "Неправильный логин или пароль";
+            return;
+        }
+        const id = result.data.body.id
+        const user = {id: "1"};
+	navbar(user);
+        window.router.redirect('/profile' + id)
+    });
 }
