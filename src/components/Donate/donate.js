09d7dee3b104c4dfcfa97472394311da01bbb1cb
dnonakolesax <90285641+dnonakolesax@users.noreply.github.com>
2023-11-06 01:27:04 +0300

    Update donate.js

--- src/components/Donate/donate.js
+++ src/components/Donate/donate.js
@@ -4,11 +4,13 @@ import {Api} from "@modules/api.js";
 export default async (receiver) => {
     const dialog = document.getElementById('donate-dialog');
     document.querySelector('#close').onclick = function () {
-        dialog.close()
+        dialog.close();
     }
-    document.querySelector('#send-donate').onclick = function () {
+    document.querySelector('#send-donate').onclick = async function () {
         const sum = document.querySelector('.sum').value;
         const api = new Api();
-        api.donate(window.user.id, receiver,'RUB', sum);
+        const response = await api.donate(window.user.id, receiver,'RUB', sum);
+        dialog.close();
+        window.location.href = response.data.body.redirect_url;
     }
 }
