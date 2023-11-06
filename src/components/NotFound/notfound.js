60458c746ec543d6d19a3812587289cdeeb24c13
dnonakolesax <90285641+dnonakolesax@users.noreply.github.com>
2023-11-06 22:54:10 +0300

    fix notfound

--- src/components/NotFound/notfound.js
+++ src/components/NotFound/notfound.js
@@ -2,6 +2,7 @@ import { ROOT_ELEMENT_ID } from '@configs/common_config.js';
 import notfound from '@components/NotFound/notfound.handlebars'
 
 import css from './not_found.css'
+import agentCooper from '@static/img/agent_cooper.png'
 
 /**
  * Функция отрисовки страницы 404
@@ -9,4 +10,6 @@ import css from './not_found.css'
 export default () => {
   const rootElement = document.querySelector(ROOT_ELEMENT_ID);
   rootElement.innerHTML = notfound();
+  const uwImg = document.getElementById('uw-img');
+  uwImg.src = agentCooper;
 };
