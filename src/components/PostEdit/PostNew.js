7190aff2dc5541f84eba4591ec8afdf22aa49909
M0rdovorot <sera.ill@yandex.ru>
2023-11-24 21:30:16 +0300

    загрузка аттачей

--- src/components/PostEdit/PostNew.js
+++ src/components/PostEdit/PostNew.js
@@ -144,7 +144,7 @@ export default async () => {
             frame.src = e.target.href;
             let x = window.open();
             x.document.open();
-            x.document.write(frame);
+            x.document.write(frame.innerHTML);
             x.document.close();
           });
           attachesEl.appendChild(doc);
