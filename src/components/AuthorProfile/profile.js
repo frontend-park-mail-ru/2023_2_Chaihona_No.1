71ef968dfa0440251fd21e1bbd2d222468d523d2
M0rdovorot <sera.ill@yandex.ru>
2023-11-28 21:11:06 +0300

    Поправить отписку

--- src/components/AuthorProfile/profile.js
+++ src/components/AuthorProfile/profile.js
@@ -141,6 +141,7 @@ export default async () => {
     const profileWithoutZero = profile;
     profileWithoutZero.subscribe_levels = profileWithoutZero.subscribe_levels.filter((e) => e.level !== 0);
     profileWithoutZero.subscribe_levels = Array.from(profileWithoutZero.subscribe_levels, (level) => {
+      level.isOwner = isOwner;
       if (level.id === profile.visiter_subscription_level_id) {
         level.has = true;
       } else {
