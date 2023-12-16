fa29ac29d9e3c9580db9aebce83e3d2ea3936b8b
M0rdovorot <sera.ill@yandex.ru>
2023-12-16 18:47:50 +0300

    Добваить отрисовку аваторок у комментов

--- src/components/Comment/comment.js
+++ src/components/Comment/comment.js
@@ -2,7 +2,7 @@ import css from '@components/Comment/comment.scss';
 
 export default async () => {
   const comments = document.querySelectorAll('.comment-ava');
-  comments.array.forEach( async (element) => {
+  comments.forEach( async (element) => {
     const userId = element.dataset.user;
     const userAva = await api.getAvatar(userId);
     element.src = userAva;
