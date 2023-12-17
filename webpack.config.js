198cccdd47368085adb7b4be6054b48f03425d46
M0rdovorot <sera.ill@yandex.ru>
2023-12-17 23:37:44 +0300

    Поправить зависимости webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const { webpack } = require('webpack');
+const webpack  = require('webpack');
 
 module.exports = [
     {
