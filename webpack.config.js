4772db97b07a0a0c2cc33516a397c8c5406ffc0e
M0rdovorot <sera.ill@yandex.ru>
2023-12-25 12:32:41 +0300

    Сжать и оптимизировать

--- webpack.config.js
+++ webpack.config.js
@@ -21,36 +21,36 @@ module.exports = [
                 {
                     test: /\.(gif|png)$/i,
                     type: 'asset/resource',
-                    use: [
-                        {
-                          loader: 'file-loader',
-                          options: {
-                            outputPath: 'images',
-                          },
-                        },
-                        {
-                          loader: 'image-webpack-loader',
-                          options: {
-                            mozjpeg: {
-                              progressive: true,
-                              quality: 65,
-                            },
-                            optipng: {
-                              enabled: false,
-                            },
-                            pngquant: {
-                              quality: [0.65, 0.90],
-                              speed: 4,
-                            },
-                            gifsicle: {
-                              interlaced: false,
-                            },
-                            webp: {
-                              quality: 75,
-                            },
-                          },
-                        },
-                    ],
+                    // use: [
+                    //     {
+                    //       loader: 'file-loader',
+                    //       options: {
+                    //         outputPath: 'images',
+                    //       },
+                    //     },
+                    //     {
+                    //       loader: 'image-webpack-loader',
+                    //       options: {
+                    //         mozjpeg: {
+                    //           progressive: true,
+                    //           quality: 65,
+                    //         },
+                    //         optipng: {
+                    //           enabled: false,
+                    //         },
+                    //         pngquant: {
+                    //           quality: [0.65, 0.90],
+                    //           speed: 4,
+                    //         },
+                    //         gifsicle: {
+                    //           interlaced: false,
+                    //         },
+                    //         webp: {
+                    //           quality: 75,
+                    //         },
+                    //       },
+                    //     },
+                    // ],
                 },
                 {test: /\.svg/, type: 'asset/inline'},
                 //{test: /\.css$/, use: ['style-loader', 'css-loader']},
