9d558d7919fa66dda796bfce2fd50c615736f2d7
dnonakolesax <90285641+dnonakolesax@users.noreply.github.com>
2023-12-28 02:18:39 +0300

    abc

--- src/modules/router.js
+++ src/modules/router.js
@@ -37,29 +37,33 @@ export class Router {
    */
   redirect(path, state = null, suffix = null) {
     const bodyElement = document.body;
-    bodyElement.classList.add('page-is-changing');
-    bodyElement.classList.add('page-not-done');
-    document.querySelector('.cd-loading-bar')
-      .addEventListener('transitionend', () => {
-        const renderer = path.replace(/[/0-9]*/g, ''); // удалить лишние цифры если ссылка связана с каким-то id (e.g. id профиля)
-        let route = routes[renderer];
-        if (!path.startsWith('blob')) {
-          window.history.pushState(state, null, path);
-          if (route === undefined) {
-            route = routes.notfound;
+    bodyElement.classList.remove('hidden');
+    setTimeout(() => {
+      bodyElement.classList.add('page-is-changing');
+      bodyElement.classList.add('page-not-done');
+      document.querySelector('.cd-loading-bar')
+        .addEventListener('transitionend', () => {
+          const renderer = path.replace(/[/0-9]*/g, ''); // удалить лишние цифры если ссылка связана с каким-то id (e.g. id профиля)
+          let route = routes[renderer];
+          if (!path.startsWith('blob')) {
+            window.history.pushState(state, null, path);
+            if (route === undefined) {
+              route = routes.notfound;
+            }
+            setTimeout(() => {
+              bodyElement.classList.remove('page-is-changing');
+            }, 1200);
+            setTimeout(() => {
+              bodyElement.classList.remove('page-not-done');
+              bodyElement.classList.add('hidden');
+            }, 1800);
+            if (renderer === 'feed') {
+              route.render(suffix);
+            } else {
+              return route.render();
+            }
           }
-          setTimeout(() => {
-            bodyElement.classList.remove('page-is-changing');
-          }, 1200);
-          setTimeout(() => {
-            bodyElement.classList.remove('page-not-done');
-          }, 1800);
-          if (renderer === 'feed') {
-            route.render(suffix);
-          } else {
-            return route.render();
-          }
-        }
-      }, { once: true });
+        }, { once: true });
+    }, 400);
   }
 }
